d2925b1e0a6cb88675bf4e36dd81dcee95ddfc87
Abdullah Ariff <abdullah.ariff@made.com>
2019-09-19 13:59:08 +0100

    Teeny refactoring

--- src/App.js
+++ src/App.js
@@ -2,6 +2,63 @@ import React from 'react';
 import './App.css';
 import { Input, Icon } from 'semantic-ui-react';
 
+const url = 'http://970c4ea3.ngrok.io/image-search';
+
+function imageSearch(image) {
+  return {
+    "matches": [
+      {
+        "SKU": "SOFSCT046BLU-ME",
+        "description": "Charley Accent Chair, Midnight Grey Velvet",
+        "image_url": "http://www.made.com/image.jpg",
+        "price": "179.00",
+        "style": "sexy",
+        "type": "seating",
+        "score": 0.987
+      },
+      {
+        "SKU": "TABSCT046RED-ME",
+        "description": "Bloody solid oak table",
+        "image_url": "http://www.made.com/image2.jpg",
+        "price": "173.00",
+        "style": "hard",
+        "type": "table",
+        "score": 0.987
+      }
+    ],
+    "hotspots": [
+      {
+        "product_type": "seating",
+        "coords": [100, 150],
+      },
+      {
+        "product_type": "table",
+        "coords": [260, 355],
+      }
+    ],
+    "tags": [
+      {
+        "name": "chairs",
+        "score": 0.90
+      },
+      {
+        "name": "armchairs",
+        "score": 0.75
+      },
+    ]
+  }
+}
+
+function ImagePreview(props) {
+  return (
+    <img 
+      src={props.url}
+      alt="The capture will appear in this box."
+      id='image-output'
+    />
+  )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -39,11 +96,7 @@ class App extends React.Component {
             value={this.state.image_url}
             onChange={this.handleChange}/>
         </header>
-        <img 
-          src={this.state.file}
-          alt="The capture will appear in this box."
-          id='image-output'
-        />
+        <ImagePreview url={this.state.file}/>
       </div>
     );
   }
