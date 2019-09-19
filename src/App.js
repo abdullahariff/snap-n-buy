60fc81d3dcc7b6a65833ca0a644539d214b93bac
Abdullah Ariff <abdullah.ariff@made.com>
2019-09-19 12:08:17 +0100

    We can render photos!

--- src/App.js
+++ src/App.js
@@ -2,26 +2,51 @@ import React from 'react';
 import './App.css';
 import { Input, Icon } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Input 
-          action={
-            <label for='image-input'>
-              <Icon name='photo' bordered inverted color='black' size='large' />
-            </label>
-          }
-          icon='search'
-          placeholder='Search...'
-          iconPosition='left'
-          className='search-bar'
-        />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {file: null};
 
-        <input type='file' accept='image/*' id='image-input'/>
-      </header>
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const url = URL.createObjectURL(e.target.files[0]);
+    this.setState({file: url})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Input 
+            action={
+              <label for='image-input'>
+                <Icon name='photo' bordered inverted color='black' size='large' />
+              </label>
+            }
+            icon='search'
+            placeholder='Search...'
+            iconPosition='left'
+            className='search-bar'
+          />
+
+          <input
+            type='file'
+            accept='image/*'
+            capture='user'
+            id='image-input'
+            value={this.state.image_url}
+            onChange={this.handleChange}/>
+        </header>
+        <img 
+          src={this.state.file}
+          alt="The capture will appear in this box."
+          id='image-output'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
