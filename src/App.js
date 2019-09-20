ddaed629ae81f15b612636c801c904f3ee249e45
Tobin Saunders <tobinsaunders@gmail.com>
2019-09-20 13:16:13 +0100

    fixed camera loading in camcorder mode, added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,55 +1,49 @@
-import React from 'react';
-import './App.css';
-import { Input, Icon, Button, Card, Image } from 'semantic-ui-react';
-import _ from 'lodash';
-import $ from 'jquery';
-import searchImage from './service'
+import React from "react";
+import "./App.css";
+import { Input, Icon, Button, Card, Image } from "semantic-ui-react";
+import _ from "lodash";
+import $ from "jquery";
+import searchImage from "./service";
 
-
-function Mast () {
-  return (
-    <img src='assets/mast.png' alt='Mast' className='mast'/>
-  )
+function Mast() {
+  return <img src="assets/mast.png" alt="Mast" className="mast" />;
 }
 
-function Home () {
-  return (
-    <img src='assets/home.png' alt='Home' className='home'/>
-  )
+function Home() {
+  return <img src="assets/home.png" alt="Home" className="home" />;
 }
 
 function ImagePreview(props) {
   return (
-    <div className='image-output-wrapper'>
-      <img 
+    <div className="image-output-wrapper">
+      <img
         src={props.url}
         alt="The capture will appear in this box."
-        id='image-output'
+        id="image-output"
       />
-      {props.hotspots && props.hotspots.map((value, index) => {
-        return (
-          <Hotspot key={index} coords={value.coords}/>
-        )
-      })}
+      {props.hotspots &&
+        props.hotspots.map((value, index) => {
+          return <Hotspot key={index} coords={value.coords} />;
+        })}
     </div>
-  )
+  );
 }
 
 function Hotspot(props) {
-  const img = document.getElementById('image-output');
+  const img = document.getElementById("image-output");
   const imageWidth = img.naturalWidth;
   const imageHeight = img.naturalHeight;
   const leftPercent = (props.coords[0] / imageWidth) * 100;
   const topPercent = (props.coords[1] / imageHeight) * 100;
   return (
     <div
-      className='hotspot'
+      className="hotspot"
       style={{
         left: `${leftPercent}%`,
-        top: `${topPercent}%`,
+        top: `${topPercent}%`
       }}
     />
-  )
+  );
 }
 
 function Tags(props) {
@@ -57,87 +51,81 @@ function Tags(props) {
     <div className="tags">
       {props.labels.map((value, index) => {
         return (
-          <Button key={index} size='large'>
+          <Button key={index} size="large">
             {value}
           </Button>
-        )
+        );
       })}
-      </div>
-  )
+    </div>
+  );
 }
 
 function Result(props) {
   let tags;
   let hotspots;
 
-  if (props.data && props.data.hasOwnProperty('tags')) {
-    tags = _.orderBy(props.data.tags, ['score'], ['desc'])
-      .map(t => t.name.charAt(0).toUpperCase() + t.name.slice(1));
+  if (props.data && props.data.hasOwnProperty("tags")) {
+    tags = _.orderBy(props.data.tags, ["score"], ["desc"]).map(
+      t => t.name.charAt(0).toUpperCase() + t.name.slice(1)
+    );
   }
-  if (props.data && props.data.hasOwnProperty('hotspots')) {
+  if (props.data && props.data.hasOwnProperty("hotspots")) {
     hotspots = props.data.hotspots;
   }
 
   return (
     <div>
-      <ImagePreview url={props.image_url} hotspots={hotspots}/>
+      <ImagePreview url={props.image_url} hotspots={hotspots} />
       <hr />
-      {tags &&
-        <Tags labels={tags} />
-      }
-      {props.data && props.data.hasOwnProperty('matches') &&
-        <ProductResults products={props.data.matches}/>
-      }
+      {tags && <Tags labels={tags} />}
+      {props.data && props.data.hasOwnProperty("matches") && (
+        <ProductResults products={props.data.matches} />
+      )}
     </div>
-  )
+  );
 }
 
 function Product(props) {
   const data = props.data;
   return (
-    <Card className='product'>
+    <Card className="product">
       <Image src={data.image_url} wrapped ui={false} />
       <Card.Content>
-        <Card.Description>
-          {data.description}
-        </Card.Description>
+        <Card.Description>{data.description}</Card.Description>
       </Card.Content>
       <Card.Content>
         <strong>£{data.price}</strong>
       </Card.Content>
     </Card>
-  )
+  );
 }
 
 function ProductResults(props) {
-  const products = _.orderBy(props.products, ['score'], ['desc'])
+  const products = _.orderBy(props.products, ["score"], ["desc"]);
   return (
-    <div className='product-results'>
+    <div className="product-results">
       {products.map((value, index) => {
-          return (
-            <Product key={index} data={value} />
-          )
-        })}
+        return <Product key={index} data={value} />;
+      })}
     </div>
-  )
+  );
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {file: null};
+    this.state = { file: null };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
-    const el = $('#image-input');
+    const el = $("#image-input");
     const file = URL.createObjectURL(e.target.files[0]);
-    this.setState({...this.state, file: file})
-    searchImage(el[0])
-      .then(result => {
-        this.setState({...this.state, data: result});
-      })
+    this.setState({ ...this.state, file: file });
+    searchImage(el[0]).then(result => {
+      this.setState({ ...this.state, data: result });
+    });
   }
 
   render() {
@@ -145,39 +133,44 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        {!this.state.file &&
-          <Mast />
-        }
+        {!this.state.file && <Mast />}
 
         <header>
-          <div className='search-bar-wrapper'>
+          <div className="search-bar-wrapper">
             <Input
               action={
-                <label for='image-input'>
-                  <Icon name='photo' bordered inverted color='black' size='large' />
+                <label for="image-input">
+                  <Icon
+                    name="photo"
+                    bordered
+                    inverted
+                    color="black"
+                    size="large"
+                  />
                 </label>
               }
-              icon='search'
-              placeholder='Search...'
-              iconPosition='left'
-              className='search-bar'
+              icon="search"
+              placeholder="Search..."
+              iconPosition="left"
+              className="search-bar"
             />
           </div>
 
           <input
-            type='file'
-            name='photo'
-            accept='image/*'
-            capture='user'
-            id='image-input'
+            type="file"
+            name="photo"
+            accept="image/*"
+            capture="camcorder"
+            id="image-input"
             value={this.state.image_url}
-            onChange={this.handleChange}/>
+            onChange={this.handleChange}
+          />
         </header>
 
         {!this.state.file ? (
           <Home />
         ) : (
-          <Result data={data} image_url={this.state.file}/>
+          <Result data={data} image_url={this.state.file} />
         )}
       </div>
     );
