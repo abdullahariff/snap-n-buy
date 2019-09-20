ec7bd8b4ab2ceef39f1571f9cbe965af4e5da12d
Abdullah Ariff <abdullah.ariff@made.com>
2019-09-20 10:50:15 +0100

    Fix image URLs and prices

--- src/service.js
+++ src/service.js
@@ -54,12 +54,13 @@ function getElastiInformation(SKUs) {
 
 function extractProductData(productData) {
   let matches = [];
+  const cloudinaryURL = "https://res-5.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/";
   productData.data.products.forEach(function(product){
     matches.push({
       SKU: product.sku,
       description: product.name,
-      image_url: product.images.listingImage,
-      price: product.price.current,
+      image_url: cloudinaryURL + product.images.listingImage,
+      price: product.price.current / 100,
       style: product.details.style,
       type: product.details.productType.value
     });
